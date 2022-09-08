2d884df82563831d892d8e014237d345537b1c9f
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-09-08 18:15:07 +0300

    creates tables for all models

---
+++ db/migrate/20220908141331_create_blogs.rb
@@ -0,0 +1,15 @@
+class CreateBlogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :blogs do |t|
+      t.string :category
+      t.string :title
+      t.string :content
+      t.string :photographer
+      t.string :caption
+      t.string :tag
+      t.string :language
+      t.integer :author_id
+      t.integer :user_id
+    end
+  end
+end
