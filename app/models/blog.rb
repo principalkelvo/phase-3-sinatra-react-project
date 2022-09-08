12da35d004dc17e3fabf7aba9a7d6c1d5d913e30
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-09-08 20:53:17 +0300

    get all blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,10 @@
 class Blog < ActiveRecord::Base
     belongs_to :user
     belongs_to :author
+
+    def self.author
+        id= self.author_id
+        author= Author.find(id)
+        author
+      end
 end
