c0ff3f9850911f98f041c7085a3ebd58224a3154
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-09-10 01:33:35 +0300

    adds faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 puts "🌱 Seeding spices..."
-blogs = ["Croissant", "Rhubarb Pie", "Black and White Cookie", "Scone", "Banana Bread"]
+# blogs = ["Croissant", "Rhubarb Pie", "Black and White Cookie", "Scone", "Banana Bread"]
 
 # t.string :category
 # t.string :title
@@ -8,16 +8,17 @@ blogs = ["Croissant", "Rhubarb Pie", "Black and White Cookie", "Scone", "Banana 
 # t.string :caption
 # t.string :tag
 # t.string :language
+# t.string :image
 # t.integer :author_id
 # t.integer :user_id
-Author.create(name: "Northside")
-Author.create(name: "Southside")
-Author.create(name: "Eastside")
-Author.create(name: "Westside")
+Author.create(name: Faker::Name.unique.name)
+Author.create(name: Faker::Name.unique.name)
+Author.create(name: Faker::Name.unique.name)
+Author.create(name: Faker::Name.unique.name)
 # Seed your database here
 Author.all.each do |author|
     3.times do 
-    Blog.create(title: blogs.sample, category: blogs.sample,content: blogs.sample, photographer: blogs.sample, caption: blogs.sample, tag:blogs.sample,language: "english",author_id: author.id, user_id: author.id)
+    Blog.create(title: Faker::Lorem.sentence, category: Faker::Book.genre ,content: Faker::Lorem.paragraphs(number: rand(5..18)).join("\n"), photographer: Faker::Name.unique.name, image: Faker::LoremFlickr.image, caption: Faker::Lorem.sentence, tag: Faker::Hobby.activity,language: "english",author_id: author.id, user_id: author.id)
     end
 end
 
